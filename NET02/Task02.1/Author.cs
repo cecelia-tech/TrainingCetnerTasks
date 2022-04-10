842bb9377609a05a957501d98d96ff13031c4b4e
cecelia-tech <vitagriciute@gmail.com>
2022-04-10 19:54:26 +0300

    Author added

--- NET02/Task02.1/Author.cs
+++ NET02/Task02.1/Author.cs
@@ -8,5 +8,24 @@ namespace Task02._1
 {
     internal class Author
     {
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public Author(string firstName, string lastName)
+        {
+            if (string.IsNullOrEmpty(firstName) || firstName.Length > 200)
+            {
+                throw new ArgumentException("First name can't be empty or longer than 200 characters");
+            }
+            FirstName = firstName;
+
+            if (string.IsNullOrEmpty(lastName) || lastName.Length > 200)
+            {
+                throw new ArgumentException("Lasnt name can't be empty or longer than 200 characters");
+            }
+            LastName = lastName;
+        }
+
     }
 }
