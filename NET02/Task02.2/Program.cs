6efe49907a48c5e9539a50077470cc239142e4ca
cecelia-tech <vitagriciute@gmail.com>
2022-04-13 11:44:03 +0300

    beginning of xml reader

--- NET02/Task02.2/Program.cs
+++ NET02/Task02.2/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+
+using (XmlReader reader = XmlReader.Create("UserSettings.xml"))
+{
+    reader.MoveToContent();
+    StringBuilder result = new StringBuilder();
+    //string result;
+    while (reader.Read())
+    {
+        //result = reader.ReadContentAsString();
+        result.Append( reader.NodeType switch
+        {
+            XmlNodeType.Element when reader.Name == "login" => $"Login: {reader.GetAttribute("name")}\n",
+            XmlNodeType.Element when reader.Name == "window" => $"{reader.GetAttribute("title")}( ",
+            XmlNodeType.Text => $"{reader.Value}, ",
+            XmlNodeType.EndElement when reader.Name == "window" => $")\n",
+            XmlNodeType.EndElement when reader.Name == "login" => "-------------------\n",
+            _ => ""
+        });
+
+        Console.Write(result.ToString());
+
+    }
+    Console.Write(result.ToString());
+}
