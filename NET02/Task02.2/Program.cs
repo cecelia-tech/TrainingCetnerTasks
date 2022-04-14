86dd53d9cd4821c0219ca1c6e814b96a08edf5c5
cecelia-tech <vitagriciute@gmail.com>
2022-04-14 11:34:09 +0300

    main count and ain children count veikia

--- NET02/Task02.2/Program.cs
+++ NET02/Task02.2/Program.cs
@@ -10,12 +10,76 @@ var purchaseOrderFilepath = Path.Combine(currentDirectory, filename);
 
 XElement userInfo = XElement.Load(purchaseOrderFilepath);
 
-IEnumerable<XElement> users = userInfo.Elements();
+//kadangi elements, atiduoda pirmus <config> vaikus, kurie yra <login> ir todel mano users yra 2
+//pasirasom "login", nes jeigu pakliutu kitokiu negu <login>, kad visko nesugadintu
+IEnumerable<XElement> users = userInfo.Elements("login");
+StringBuilder userInfoToDisplay2 = new StringBuilder();
+
+//mano users yra 2 login elementai su viskuo viduje medzio atzvilgiu, ne plokscia
+
+bool CountUserMain(XElement user)
+{
+        return user.Elements("window").Where(window => window.Attribute("title").Value == "main").Select(window => window).Count() == 1;
+}
+
+//cia patikrinam ar window turi 4 elementus arba nei vieno
+bool CountElementsInMain(XElement user)
+{
+    //cia gaunam bendra vaiku skaiciu is 1 window main 
+    var d = user.Elements("window").Where(window => window.Attribute("title").Value == "main").Select(window => window).Descendants().Count();
+    return (d == 4 || d == 0);
+}
+
+foreach (var item in users)
+{
+    var a = CountUserMain(item);
+    var b = CountElementsInMain(item);
+}
+
+
+
+
+
+//users.Select(user => user.Attributes("title")).Count();
+
+
+//foreach (var user in users.Select(user => user.Attributes("main")))
+//{
+//    Console.WriteLine(user);
+//}
+//users.Select(user => user.Attributes("main").ToList().ForEach(windowSuMain => Console.WriteLine(windowSuMain)));
+//void IsCorrect()
+//{
+//return users.Select(x => x.Elements("window").Attributes("title")).Where(x => x).Count();
+//return users.Where(x => x.Elements("window").Attributes("title").Value == "main").Select(m => m.Elements("window")).Count();
+//visiWindowSuTitle.Where(x => x.V
+//return users.Select(x => x.Attributes("title").
+//Where(x => x.Value == "main" && x.Value == "help")).Count();
+//return users.Descendants("login").Where(x => x.Elements("window").Contains(x.Element("main"))).Select(xm => xm.Element("window")).Count();
+//users.Where(x => x.Name == "window").Select(x => x.Attributes("main"));
+
+    
+//}
+
+
+//bool IsCorrect()
+//{
+//    foreach (var item in users.Select(x => x.Elements("window")))
+//    {
+//        return item.Attributes("title").Where(x => x.Value == "main").Count() == 1;
+//    }
+//}
+
+//Console.WriteLine(IsCorrect());
 //IEnumerable<string> partNos = purchaseOrder.Descendants("login").Select(x => (string)x.Attribute("name"));
 
-//users.Select(x => x.Elements());
+//users.Select(user => (user.Attribute("name").Value, ));
+/*users.ToList().ForEach(x => userInfoToDisplay2.
+Append($"Login: {x.Attribute("name").Value} \n").
+Append($" {x.Elements("window").ToList().ForEach(xm => xm.Attribute("title").Value)}").*/
+//Append(""));
 
-foreach (var user in users)
+/*foreach (var user in users)
 {
     StringBuilder userInfoToDisplay = new StringBuilder();
 
@@ -24,9 +88,10 @@ foreach (var user in users)
     //reikia user.Elements("window").Attributes("title") kad rasytu window pavadinima
     foreach (var userDetails in user.Elements("window"))
     {
+        userInfoToDisplay.Append($" {userDetails.Attribute("title").Value}(");
         //IEnumerable
         var x = userDetails.Descendants();
-        //galimai i for loopa arba kazka su contains
+       
         foreach (var item in x)
         {
             //cia kol kas sudeda visus skaicius i viena eilute
@@ -34,12 +99,13 @@ foreach (var user in users)
 
         }
         //userInfoToDisplay.Append($"{userDetails.Value}");
+        userInfoToDisplay.Append(")\n");
         //Console.WriteLine(userDetails.Value);
     }
     //Console.WriteLine(user.Attribute("name").Value);
     Console.WriteLine(userInfoToDisplay.ToString());
-}
-
+}*/
+//Console.WriteLine(userInfoToDisplay2.ToString());
 
 
 /*using (XmlReader reader = XmlReader.Create("UserSettings.xml"))
