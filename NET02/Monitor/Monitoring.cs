4d771405e2ad035f185de78e03acb219ad155cb6
cecelia-tech <vitagriciute@gmail.com>
2022-04-27 13:43:51 +0300

    monitoring added

--- NET02/Monitor/Monitoring.cs
+++ NET02/Monitor/Monitoring.cs
@@ -1,7 +1,99 @@
-﻿namespace Monitor
+﻿using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace Monitor
 {
     public class Monitoring
     {
+        public Settings? MonitoringSettings { get; set; }
+        public System.Timers.Timer? aTimer { get; set; }
+        HttpClient? client;
+        public Monitoring()
+        {
+            var JsonSettings = JsonConvert.DeserializeObject<JObject>(File.ReadAllText("WebsiteSettings.json"));
 
+            MonitoringSettings = JsonSettings?.ToObject<Settings>();
+        }
+
+        public void SetTimer()
+        {
+            aTimer = new System.Timers.Timer(1000);
+            aTimer.Elapsed += async (obj, e) => await OnTimedEvent();
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+            //aTimer.Start();
+            GC.KeepAlive(aTimer);
+        }
+
+        private async Task OnTimedEvent()
+        {
+            client = new HttpClient();
+            var stopWatch = new Stopwatch();
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            foreach (var url in MonitoringSettings?.Sites)
+            {
+                stopWatch = Stopwatch.StartNew();
+                response = await client.GetAsync(url);
+                stopWatch.Stop();
+            }
+
+
+            var timeForResponse = stopWatch.Elapsed;
+            await CheckStatus(response.IsSuccessStatusCode, timeForResponse, response.Content);
+        }
+
+        public async Task CheckStatus(bool webResponse, TimeSpan timeForResponse, HttpContent url)
+        {
+            if (webResponse &&
+                timeForResponse <= MonitoringSettings?.ResponceTime
+                )
+            {
+                //Logger loger = new Logger();
+                Console.WriteLine("writing to the log");
+                //Console.WriteLine(url.ToString().Length);
+            }
+            else
+            {
+                await SendEmail();
+            }
+        }
+
+
+
+        public async Task SendEmail()
+        {
+            await Task.Delay(500);
+            Console.WriteLine("we are on the SendEmail");
+            //sitas metodas turi but ASYNK AWAIT for sure   
+        }
+
+
+
+
+
+
+        //sita darysim veliau
+        public void WatchFile()
+        {
+            using (var watcher = new FileSystemWatcher
+            {
+                Path = MonitoringSettings?.Path,
+                Filter = "WebsiteSettings.json"
+            })
+            {
+                //NEED TO THINK ABOUT DIFFERENT METHODS HOW IT WILL REACT TO THE CHANGES
+                //watcher.Changed += someEvent;
+                //watcher.Created += someEvent;
+                //watcher.Deleted += someEvent;
+                //watcher.Renamed += someEvent;
+                //watcher.Error += someEvent;
+
+                //watcher.EnableRaisingEvents = true;
+                //Console.ReadLine();
+
+                //watcher.EnableRaisingEvents = false;
+            }
+        }
     }
 }
