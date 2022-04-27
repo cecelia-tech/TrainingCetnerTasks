2920448867e30b42d7d4227871fabb8f644ebe82
cecelia-tech <vitagriciute@gmail.com>
2022-04-27 13:40:32 +0300

    task02.4 initial commit

---
+++ NET02/MonitoringApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using Monitor;
+
+using (var mutex = new Mutex(false, "SingletonApp"))
+{
+    // TimeSpan.Zero to test the mutex's signal state and
+    // return immediately without blocking
+    bool isAnotherInstanceOpen = !mutex.WaitOne(TimeSpan.Zero);
+    if (isAnotherInstanceOpen)
+    {
+        Console.WriteLine("Only one instance of this app is allowed.");
+        //Console.Read();
+        Thread.Sleep(2000);
+        return;
+    }
+
+    Monitoring mon = new Monitoring();
+    //Monitoring mon = new Monitoring();
+    //mon.SetTimer();
+
+    Console.ReadKey();
+
+    mutex.ReleaseMutex();
+}
