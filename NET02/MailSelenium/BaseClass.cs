e5665dc14be47aa8a462236e99a75cfcaa70e874
cecelia-tech <vitagriciute@gmail.com>
2022-05-18 09:47:46 +0300

    Initial Commit

---
+++ NET02/MailSelenium/BaseClass.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSelenium;
+
+internal class BaseClass
+{
+    internal IWebDriver _driver;
+    public BaseClass(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    public IWebElement GetElementByXPath(string path)
+    {
+        IWebElement? webElement = null;
+
+        while (webElement == null)
+        {
+            try
+            {
+                webElement = _driver.FindElement(By.XPath(path));
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        return webElement;
+    }
+}
---
+++ NET02/TestMailSelenium/Usings.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using OpenQA.Selenium;
+global using OpenQA.Selenium.Chrome;
+global using System.Threading;
